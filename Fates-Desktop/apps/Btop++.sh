c1f2942417ec522ef68d1d34d328a9b67eda2b1f
Fate <35616898+FateSwap@users.noreply.github.com>
2022-09-17 12:44:25 -0400

    Delete Fates-Desktop directory

--- Fates-Desktop/apps/Btop++.sh
+++
@@ -1,7 +0,0 @@
-/! /bin/sh
-
-# btop++, a nice looking htop can be installed by
-git clone https://github.com/aristocratos/btop.git
-cd btop
-sudo make
-sudo make install
--- Fates-Desktop/apps/yay.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-# installs yay from aur, yay is an aur helper
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
